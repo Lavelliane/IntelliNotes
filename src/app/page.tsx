497030c84a6de5f889c877654be64d27275b39f5
Lavelliane <jhurylastre432@gmail.com>
2024-02-03 20:05:46 +0800

    change redirec

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import { auth } from "@clerk/nextjs";
 import { Button } from "antd";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
 export default function Home() {
+
+  const { userId } : { userId: string | null } = auth();
+
+  if(userId){
+    redirect('/home')
+  }
+
   return (
     <main>
       Landing page coming soon!
