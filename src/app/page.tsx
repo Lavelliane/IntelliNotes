0d03a5fc3226d924bc95bf7f489437771ed09a8a
Lavelliane <jhurylastre432@gmail.com>
2024-02-26 12:19:11 +0800

    add tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,8 @@
-import { auth } from "@clerk/nextjs";;
-import { redirect } from "next/navigation";
-import styles from './page.module.css'
-
-export default function Home() {
-
-  const { userId } : { userId: string | null } = auth();
-
-  if(userId){
-    redirect('/home')
-  }
+export default function Landing() {
 
   return (
     <main>
-      <h1 className={styles.title}>Hello</h1>
+      <h1 className="text-2xl">Hello</h1>
     </main>
   );
 }
