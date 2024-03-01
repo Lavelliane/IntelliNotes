c06f2ad92c6da1f7c4b5728e717d6785d0bb3c59
chwello <fretchelm.gerarman@gmail.com>
2024-03-01 22:15:08 +0800

    fix width

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { useEffect } from "react";
 export default function Landing() {
 
   return (
-    <main className="h-screen">
+    <main className="h-screen overflow-x-hidden">
       <header className="text-white flex items-center justify-between p-4 font-sans mt-[7px]">
         <div className="items-center flex ml-32">
           <img src="logo.svg" alt="logo" className="logo w-10 h-16 z-[-0]" />
